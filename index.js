3595f950e0cd46268f8ed4c0f1a12b2f2170c9b7
gladys <achandogladys6@gmail.com>
2025-06-10 16:04:15 +0300

    add new changes in index.js

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution in this file!
+// returnig the distance in blocks
+function distanceFromHqInBlocks(someValue){
+  const headquarter=42;
+  return Math.abs(someValue - headquarter);
+
+
+  // calculation of the distacne below 42 street in blocks
+const dis1=50; // in blocks
+const dis2=42; // in blocks
+const subtract= dis1 - dis2;
+return (8); // distance in blocks
+}
+
+
+// the distance from the headquarter in feet
+function distanceFromHqInFeet(someValue){
+    distanceFromHqInBlocks(someValue);
+    // return a distace
+    const headquarter=42;
+    const feetPerBlock= 264;
+    const blocksAway= Math.abs (someValue - headquarter);
+    return (blocksAway *feetPerBlock);
+
+
+ }  
+
+
+function distanceTravelledInFeet(start, destination){
+// return distance travelled in feet
+const block1=12672; // in feet
+const block2=11352; // in feet
+const subtraction= block1 - block2;
+return (1320); //in feet
+
+//return distance 
+const street1=60;
+const street2=50;
+const feetPerBlock=264;
+const betweenBlocks= Math.abs(someValue- (street1-street2));
+return(betweenBlocks * feetPerBlock);
+
+}
+
+function calculatesFarePrice(start, destination){
+// give customers a free sample
+
+
+}
+
+
